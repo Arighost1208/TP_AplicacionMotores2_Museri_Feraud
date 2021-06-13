4efe0df9612ca92236a9c5574b75c6ed7ab3fa72
LucasFeraud <84747980+LucasFeraud@users.noreply.github.com>
2021-06-13 13:05:32 -0300

    Preview
    Se creo un preview de los waypoints utilizando un personaje como patrulla, donde este recorre cada uno de los puntos que seleccionamos al darle play.

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    
+    public float patrolSpeed = 0f;
+    public float changeTargetDistance = 0.1f;
+    public Transform[] patrolWaypoints;
+
+    int currentTarget = 0;
+   
+    void Update()
+    {
+        if (MoveToTarget())
+        {
+            currentTarget = GetNextTarget();
+        }
+    }
+
+    private bool MoveToTarget()
+    {
+        Vector3 distanceVector = patrolWaypoints[currentTarget].position - transform.position;
+        if(distanceVector.magnitude < changeTargetDistance)
+        {
+            return true;
+        }
+
+        Vector3 velocityVector = distanceVector.normalized;
+        transform.position += velocityVector * patrolSpeed * Time.deltaTime;
+
+        return false;
+    }
+
+    private int GetNextTarget()
+    {
+        currentTarget++;
+
+        if(currentTarget >= patrolWaypoints.Length)
+        {
+            currentTarget = 0;
+        }
+
+        return currentTarget;
+    }
+}
